ae95eb6a58de89697f6485a8e484c2df22743341
Nishant Gopalakrishnan <ngopalak@fhcrc.org>
2011-03-25 22:26:56 +0000

    Updates to the unit tests to reflect change in the API
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Streamer@54058 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/testNcdf.R
+++ inst/unitTests/testNcdf.R
@@ -1,66 +1,82 @@
 
 test_2D_Data_read <- function() {
     file <- system.file("extdata", "NetCDFData.nc", package = "Streamer")
-    ncprod <- NetCDFProducer(file)
-    vars <- variableNames(ncprod)
+    ncFile <- NetCDFFile(file)
+
+    vars <- names(ncFile)
     checkIdentical(vars, c( "2dIntData", "2dFloatData"))
 
-    sliceDimensions(ncprod, "2dIntData") <- list(sampleDim = 5, snpDim =5)
-    current <- sliceDimensions(ncprod, "2dIntData")
-    target <- structure(c(5,5), names = c( "sampleDim", "snpDim"))
-    checkEquals(target, current)
+    current <- dimensions(ncFile)
+    target <- list("2dIntData" = c("sampleDim", "snpDim"),
+                   "2dFloatData" = c("sampleDim", "snpDim" ))
+    checkIdentical(current, target)
+    
+    current <- dimensionLengths(ncFile)
+    nms <-  c("sampleDim", "snpDim")
+    target <- list("2dIntData" = structure(c(5L, 10L), names = nms) ,
+                   "2dFloatData" = structure( c(5L, 10L), names = nms))
+    checkIdentical(current, target)
 
-    dat <- yield(ncprod, "2dIntData")
-    current <- status(ncprod, "2dIntData")
+    ncprod <- NetCDFProducer(ncFile, "2dIntData")
+    current <- names(ncprod)
+    checkIdentical(current, "2dIntData")
+    
+    slice(ncprod) <- list(snpDim = 5, sampleDim = 5)
+    current <- slice(ncprod)
+    nms <-  c("sampleDim", "snpDim")
+    target <-  structure( c(5, 5), names = nms)
+    checkIdentical(current, target)
+
+    dat <- yield(ncprod)
+    current <- status(ncprod)
     target <- structure( c(1,6), names = c("sampleDim", "snpDim"))
     checkEquals(target, current)
-    
-    reset(ncprod, "2dIntData")
-    current <- status(ncprod, "2dIntData")
+   
+    reset(ncprod)
+    current <- status(ncprod)
     target <- structure( c(1,1), names = c("sampleDim", "snpDim"))
     checkEquals(target, current)
 
-    current <- yield(ncprod, "2dIntData")
+    current <- yield(ncprod)
     target <- matrix(1:25, ncol = 5)
     checkEquals(target ,current)
    
-    current <- yield(ncprod, "2dIntData")
+    current <- yield(ncprod)
     target <- matrix(26:50, ncol = 5)
     checkEquals(target ,current)
 
-    current <- yield(ncprod, "2dIntData")
+    current <- yield(ncprod)
     target <- matrix(numeric(0), 0, 0)
     checkEquals(target ,current)
   
-    reset(ncprod, "2dIntData")
-    sliceDimensions(ncprod, "2dIntData") <- list(sampleDim = 4, snpDim = 4)
+    reset(ncprod)
+    slice(ncprod) <- list(sampleDim = 4, snpDim = 4)
 
-    current <- yield(ncprod, "2dIntData")
+    current <- yield(ncprod)
     target <- matrix( c(1:4, 6:9, 11:14, 16:19), ncol = 4)
     checkEquals(target ,current)
     
-    current <- yield(ncprod, "2dIntData")
+    current <- yield(ncprod)
     target <-  c(5,10, 15, 20)
     checkEquals(target , as.numeric(current))
     
-    current <- yield(ncprod, "2dIntData")
+    current <- yield(ncprod)
     target <- c(21:24, 26:29, 31:34, 36:39)
     checkEquals(target , as.numeric(current))
-    current <- yield(ncprod, "2dIntData")
+    current <- yield(ncprod)
     target <-  c(25, 30, 35, 40)
     checkEquals(target , as.numeric(current))
 
-    current <- yield(ncprod, "2dIntData")
+    current <- yield(ncprod)
     target <- matrix(c(41: 44, 46:49), ncol =2)
     checkEquals(target ,current)
     
-    current <- yield(ncprod, "2dIntData")
+    current <- yield(ncprod)
     target <- c(45, 50)
     checkEquals(target , as.numeric(current))
     
-    current <- yield(ncprod, "2dIntData")
+    current <- yield(ncprod)
     target <- matrix(numeric(0), 0, 0)
     checkEquals(target ,current)
-
 }
 
