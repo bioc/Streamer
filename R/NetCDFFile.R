9a3120fe2ef01883ce4e23b3240c195f2cddd921
Nishant Gopalakrishnan <ngopalak@fhcrc.org>
2011-03-30 17:58:27 +0000

    Updates to NetCDF stuff
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Streamer@54251 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/NetCDFFile.R
+++ R/NetCDFFile.R
@@ -7,9 +7,10 @@
                   numDims = "integer"))
 
 .NetCDFFile$methods(
-    initialize = function(file = NULL, ...) {
+     initialize = function(file = NULL,...) {
+        
         "Initialize all the fields of the NetCDFFile class"
-        if(!is.null(file)) {
+        if (!is.null(file)) {
             .self$con <- nc_open(file)
             .self$variableNames <- .getNcdfVariableNames(con)
             .self$dimensionNames <- .getNcdfDimNames(con)
@@ -18,49 +19,77 @@
             .self$precision <- .getNcdfPrecision(con)
         }
         .self
+    
     },
     getVariableNames = function() {
+
         "Returns the names for all variables in the file"
         variableNames
+
     },
     getDimensionNames = function() {
+    
         "Returns the names of the dimensions "
         .getFieldValue(.self, "dimensionNames")
+    
     }, 
     getDimensionLengths = function() {
+
         "Returns the dimension lengths"
         .getFieldValue(.self, "dimensionLengths")
+    
     },
     getDimensionCounts = function() {
+
         "Returns the number of dimensions"
         .getFieldValue(.self, "numDims")
+    
     },
     getPrecision = function() {
+        
         "Returns the storage precision for a variable"
         .getFieldValue(.self, "precision") 
+
     },
-    finalize = function(){
-        if(!is(con, "uninitializedField"))
+    finalize = function() {
+        
+        "Closes the NetCDF file"
+        if (!is(con, "uninitializedField"))
            nc_close(con)
+    
     }
 )
 
+
+## Constructor for NetCDFFile class
+NetCDFFile <- function(file = NULL, ... ) {
+
+    .NetCDFFile$new(file = file, ...)
+
+}
+
+
 ## retrieves the field from the NetCDFFile class
 .getFieldValue <- function(self, field) {
+
     return(self[[field]])
+
 }
 
 ## Helper for precision and number of dims
 .getNcdfInfo <- function(nc, info) {
+
     vars <- .getNcdfVariableNames(nc)
     structure(sapply(seq_len(length(vars)), function(i) {
                      nc$var[[i]][[info]]
 
     }), names = vars)
+
 }
 
 ## Helper for dimension names, dim lengths
 .getNcdfDimInfo <- function(nc,info ) {
+
     vars <- .getNcdfVariableNames(nc)
     get_dim_info <- function(varid, info) {
         ndims <- nc$var[[varid]]$ndims
@@ -71,7 +100,7 @@
     structure(
         lapply(vars, function(i) {
                val <- get_dim_info(i, info)
-               if(info == "len") {
+               if (info == "len") {
                    ndims <- nc$var[[i]]$ndims
                    nms <- sapply(seq_len(ndims), function(j) {
                                  nc$var[[i]]$dim[[j]]$name
@@ -80,39 +109,43 @@
                 }
                 val
         }), names = vars)
+
 }
 
 ## gets  names of variables in NetCDF file  
 .getNcdfVariableNames <- function(nc) {
+
     names(nc$var)
+
 }
 
 
 ## gets the dimension names for all variables as a named list
 .getNcdfDimNames <- function(nc) {
+
     .getNcdfDimInfo(nc, "name")
+
 }
 
 ## gets the dimension lengths for all variables as a named list
 .getNcdfDimLengths  <- function(nc) {
+
     .getNcdfDimInfo(nc,"len")
+
 }
 
 
 ## gets the number of dimensions for all variables in the file
 .getNcdfNumDims <- function(nc) {
+
     .getNcdfInfo(nc, "ndims")
+
 }
 
 ## gets precision info for all variables in file 
 .getNcdfPrecision <- function(nc) {
+
     .getNcdfInfo(nc, "prec")
 
 }
-
-## Constructor for NetCDFFile class
-NetCDFFile <- function(file) {
-    .NetCDFFile$new(file)
-}
-
 
--- R/NetCDFProducer.R
+++ R/NetCDFProducer.R
@@ -11,34 +11,44 @@
 
 .NetCDFProducer$methods(
     initialize  = function(ncdf, var, ...) {
-        "Initialize slots of netCDFSampler class for all variables
-        in file "
-        if(is.null(var))
+   # initialize  = function(...) {
+
+        "Initialize the fields netCDFSampler class "
+        callSuper(...)
+        if (verbose) msg(".NetCDFProducer$initialize()")
+        if (is.null(var))
             var <- names(ncdf)[1]
         .self$name <- var
         .self$slice <- .self$count <- .getDefaultSlice(ncdf, var) 
         .self$start <- .initializeStart(ncdf, var)
         .self$ncdf <- ncdf
-        .self 
+        .self
+
     },
     getSlice = function() {
+
         "Returns the slice dimensions"
-        slice                        
+        if (verbose) msg(".NetCDFProducer$getSlice()")
+        slice
+
     },
     setSlice = function(dim, ...) {
+
+        "Sets the slice dimensions"
+        if (verbose) msg(".NetCDFProducer$setSlice()")
         .checkDimension <- function(dim) {
             numDim <- dimensionCount(ncdf)[[name]]
             dimNames <- dimensions(ncdf)[[name]]
             dimLength <- dimensionLengths(ncdf)[[name]]
-            if(length(dim) != numDim)
+            if (length(dim) != numDim)
                 stop(paste("'dim' should have the same length as the number of dimensions of", name, sep =" "))
  
             nms <- names(dim) %in% dimNames
-            if(!(all(nms) && length(nms)))
+            if (!(all(nms) && length(nms)))
                 stop(paste("'dim' should have names corresponding to the names of dimensions for the variable",
                            name , sep =" "))
             sapply(seq_len(numDim), function(i) {
-                   if( dim[i] < 1 || dim[i] > dimLength[i])
+                   if ( dim[i] < 1 || dim[i] > dimLength[i])
                        stop(paste("Slice dimension for", dimNames[i],  "should be within the range 1 and", dimLength[i], sep = " "))
                   }
             )
@@ -46,27 +56,37 @@
         .checkDimension(dim) 
         .self$start <- .initializeStart(ncdf, name)
         .self$slice <- .self$count <- dim[dimensions(ncdf)[[name]]]
+
     }, 
     status = function() {
+    
         "Gets the current start position of the slice being read"
-        if(all(start == -1))
+        if (verbose) msg(".NetCDFProducer$status()")
+        
+        if (all(start == -1))
             return(dimensionLengths(ncdf)[[name]])
         else
             return(start)
+    
     },
     reset = function() {
+    
         "Resets the position to be read to start of the file"
+        callSuper()
+        if (verbose) msg(".NetCDFProducer$reset()")
         .self$start <- .initializeStart(ncdf, name)
         .self$count <- slice
+    
     },
     yield = function() {
+
         "Reads a slice. Repeated calls retrieves the next chunk of data until 
         the end of file has been reached."
+        if (verbose) msg(".NetCDFProducer$yield()")
         dimLen <- dimensionLengths(ncdf)[[name]]
-        if( all(start == -1)) {
+        if ( all(start == -1)) {
             message("Reached the end of the file ")
             return(matrix(numeric(0), 0, 0))   
-
         }
         nd <- dimensionCount(ncdf)[[name]]
         count <<- .getCurrentCount(start, slice, dimLen, count)
@@ -79,42 +99,46 @@
 
 
 ## Constructor for NetCDF producer class
-NetCDFProducer <- function(ncdf, var = NULL) {
+NetCDFProducer <- function(ncdf, var = NULL, ..., verbose = FALSE) {
    
-    if(!is(ncdf, "NetCDFFile") || missing(ncdf))
+    if (!is(ncdf, "NetCDFFile") || missing(ncdf))
         stop(paste(ncdf, "should be a valid object of class \"NetCDFFile\"", sep = " "))
     
-    if(!is.null(var)) {
-        if(!var %in% names(ncdf))
+    if (!is.null(var)) {
+        if (!var %in% names(ncdf))
             stop("var must be one of the  variables in the NetCDF file")
     }
     
-    if(length(var) != 1)
+    if (length(var) != 1)
         stop("var must specify a single variable")
-    .NetCDFProducer$new(ncdf, var)
+    .NetCDFProducer$new(ncdf = ncdf, var = var, ... , verbose = verbose)
+
 }
 
 ## Helper functions for NetCDF producer class
 
 ## sets the default slices for the getSlice function
 .getDefaultSlice <- function(nc, var) {
+
     slicePercent = 0.05 
     dimLen <- dimensionLengths(nc)[[var]]
     numDims <- dimensionCount(nc)[[var]]
     dimNames <- dimensions(nc)[[var]]
-    if(numDims ==2) {
+    if (numDims ==2) {
         mx <- max(round(dimLen[2] * slicePercent), 1)
         return(structure(c(dimLen[1], mx ), names = dimNames))
-    } else if(numDims == 3){
+    } else if (numDims == 3){
         mx <- max(round(dimLen[3] * slicePercent), 1)
         return(structure(c(dimLen[1], dimLen[2], mx ), names = dimNames))
     } else {
         structure(rep(NA, numDims), names = diimNames)
     }
+
 }
 
 ## default start value for the cursor in NetCDF file
 .initializeStart <- function(nc, var) {
+
     dimNames <- dimensions(nc)[[var]]
     dimLen <- dimensionLengths(nc)[[var]]
     numDims <- dimensionCount(nc)[[var]]
@@ -125,32 +149,36 @@ NetCDFProducer <- function(ncdf, var = NULL) {
 ## Helper functions for yield function
 ## Gets the start position for the next read
 .getNextStart <- function(start, slice, dimLen , nd) {
+
     i <- 1
     new_start <- start
     while (i <= nd) {
         if ( start[i] + slice[i]  == dimLen[i] +1) {
             new_start[i] <- 1
             i <- i +1
-        } else if(start[i] + slice[i] <= dimLen[i]) {
+        } else if (start[i] + slice[i] <= dimLen[i]) {
             new_start[i] <- slice[i] + start[i] 
             i <- nd +1
-        } else if(start[i] + slice[i]  > dimLen[i] +1) {
+        } else if (start[i] + slice[i]  > dimLen[i] +1) {
             new_start[i] <- 1
             i <- i +1
         }
     }
-    if(all(new_start == 1))
+    if (all(new_start == 1))
         new_start <- structure(rep(-1, nd), names = names(start))
     new_start
+
 }
 
 ## Gets the count to be read for current yield being processed
 .getCurrentCount <- function(start, slice, dimLen, count) {
+
     val <-  start + slice -1  
     indx <- val <= dimLen
     count[indx] <- slice[indx]
     tmp <- dimLen - start +1
     count[!indx] <- tmp[!indx]
     count                          
+
 }
 
