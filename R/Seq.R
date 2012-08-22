37dad119c55d5545e8dc939589cdbc4ed939f462
Martin Morgan <mtmorgan@fhcrc.org>
2012-08-22 02:28:14 +0000

    minor performance improvements
    - avoid is() / as() coercion from integer -> numeric in field setter
    - avoid unnecessary addition
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Streamer@68700 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Seq.R
+++ R/Seq.R
@@ -3,17 +3,21 @@
       from="numeric", to="numeric", by="numeric", length.out="integer"),
     contains="Producer",
     methods = list(
+      initialize = function(..., length.out) {
+          ## increment length.out to avoid repeated addition
+          callSuper(..., length.out=as.integer(length.out) + 1L)
+      },
       yield = function() {
           if ((from - to) * by > 0)
-              return(integer())
-          s <- seq(from, by=by, length.out=length.out + 1L)
-          .self$from <- s[length(s)]
+              return(numeric())
+          s <- seq(from, by=by, length.out=length.out)
+          from <<- as.numeric(s[length(s)])
           s <- s[-length(s)]
           s[s <= to]
       },
       show = function() {
           cat("from:", from, "\nto:", to, "\nby:", by,
-              "\nlength.out:", length.out, "\n")
+              "\nlength.out:", length.out - 1L, "\n")
       }))
 
 Seq <-
